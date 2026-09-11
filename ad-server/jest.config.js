923473f976e13c6afe9f74380047672a496985e0
Khuzaima Ahmed <khuzaimai.design@gmail.com>
2026-09-11 21:44:21 +0500

    test(demo): isolate shared emulator suites

--- ad-server/jest.config.js
+++ ad-server/jest.config.js
@@ -17,6 +17,8 @@ export default {
     },
     testMatch: ['**/tests/**/*.test.js'],
     testPathIgnorePatterns: ['/node_modules/', '/node_modules_old/'],
+    // Emulator-backed suites mutate one shared project and must not overlap.
+    maxWorkers: 1,
     verbose: true,
     forceExit: true
 };
